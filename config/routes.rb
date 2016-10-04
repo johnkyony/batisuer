f60d726a0c6578d4aa542650186f98b785599b74
jkyony <jkyony@gmail.com>
2016-10-05 00:10:31 +0100

    the fix

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,9 @@ Rails.application.routes.draw do
   resources :grades do 
     resources :streams
   end
+  resources :fees do 
+    resources :accounts
+  end
   resources :grades do
     resources :fees 
   end
