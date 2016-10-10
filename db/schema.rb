c73a36abcf07c2fac8653484c1fc2dba5db33547
John kyony <jkyony@gmail.com>
2016-10-10 18:47:01 +0200

    the focus

--- db/schema.rb
+++ db/schema.rb
@@ -32,74 +32,6 @@ ActiveRecord::Schema.define(version: 20161005000642) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "double_entry_account_balances", force: :cascade do |t|
-    t.string   "account",    limit: 31, null: false
-    t.string   "scope",      limit: 23
-    t.integer  "balance",               null: false
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
-  end
-
-  add_index "double_entry_account_balances", ["account"], name: "index_account_balances_on_account"
-  add_index "double_entry_account_balances", ["scope", "account"], name: "index_account_balances_on_scope_and_account", unique: true
-
-  create_table "double_entry_line_aggregates", force: :cascade do |t|
-    t.string   "function",   limit: 15, null: false
-    t.string   "account",    limit: 31, null: false
-    t.string   "code",       limit: 47
-    t.string   "scope",      limit: 23
-    t.integer  "year"
-    t.integer  "month"
-    t.integer  "week"
-    t.integer  "day"
-    t.integer  "hour"
-    t.integer  "amount",                null: false
-    t.string   "filter"
-    t.string   "range_type", limit: 15, null: false
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
-  end
-
-  add_index "double_entry_line_aggregates", ["function", "account", "code", "year", "month", "week", "day"], name: "line_aggregate_idx"
-
-  create_table "double_entry_line_checks", force: :cascade do |t|
-    t.integer  "last_line_id", null: false
-    t.boolean  "errors_found", null: false
-    t.text     "log"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-  end
-
-  create_table "double_entry_line_metadata", force: :cascade do |t|
-    t.integer  "line_id",               null: false
-    t.string   "key",        limit: 48, null: false
-    t.string   "value",      limit: 64, null: false
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
-  end
-
-  add_index "double_entry_line_metadata", ["line_id", "key", "value"], name: "lines_meta_line_id_key_value_idx"
-
-  create_table "double_entry_lines", force: :cascade do |t|
-    t.string   "account",         limit: 31, null: false
-    t.string   "scope",           limit: 23
-    t.string   "code",            limit: 47, null: false
-    t.integer  "amount",                     null: false
-    t.integer  "balance",                    null: false
-    t.integer  "partner_id"
-    t.string   "partner_account", limit: 31, null: false
-    t.string   "partner_scope",   limit: 23
-    t.integer  "detail_id"
-    t.string   "detail_type"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-  end
-
-  add_index "double_entry_lines", ["account", "code", "created_at"], name: "lines_account_code_created_at_idx"
-  add_index "double_entry_lines", ["account", "created_at"], name: "lines_account_created_at_idx"
-  add_index "double_entry_lines", ["scope", "account", "created_at"], name: "lines_scope_account_created_at_idx"
-  add_index "double_entry_lines", ["scope", "account", "id"], name: "lines_scope_account_id_idx"
-
   create_table "fees", force: :cascade do |t|
     t.integer  "grade_id"
     t.integer  "price"
@@ -117,15 +49,6 @@ ActiveRecord::Schema.define(version: 20161005000642) do
   end
 
   add_index "grades", ["direction_id"], name: "index_grades_on_direction_id"
-
-  create_table "invoicings", force: :cascade do |t|
-    t.integer  "student_id"
-    t.integer  "received_amount"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-  add_index "invoicings", ["student_id"], name: "index_invoicings_on_student_id"
 
   create_table "parents", force: :cascade do |t|
     t.string   "name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,52 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-# Parent.delete_all 
-# Student.delete_all
-# Fees.delete_all
+admin = User.create!(name: "admin",role:5, email: "admin@admin.com",password:"johnte12")
+finances = User.create!(name: "finances ",role: 3 , email: "finances@finances.com",password:"johnte12")
+administration = User.create!(name: "administration",role:1, email: "administration@administration.com",password:"johnte12")
+cashier= User.create!(name: "cashier",role:2, email: "cashier@cashier.com",password:"johnte12")
+Parent.delete_all 
+Student.delete_all
+Fee.delete_all
+Grade.delete_all
+Account.delete_all
+Receipt.delete_all
+    50.times do |i|
+            Parent.create!(
+                 name: Faker::Name.first_name,
+                 surname: Faker::Name.last_name,
+                 phone_number: Faker::PhoneNumber.cell_phone)
+             end
 
-# Grade.delete_all
-# Account.delete_all
-# Receipt.delete_all
-# 50.times do |i|
-#   Parent.create!(
-#     name: Faker::Name.first_name,
-#     surname: Faker::Name.last_name,
-#     phone_number: Faker::PhoneNumber.cell_phone)
-# end
+        50.times do |i|
+             Student.create!(
+                  name: Faker::Name.first_name,
+                  surname: Faker::Name.last_name,
+                  parent_id:"{i+1}")
+              end
 
-# 50.times do |i|
-#   Student.create!(
-#     name: Faker::Name.first_name,
-#     surname: Faker::Name.last_name,
-#     parent_id:"#{i+1}")
-# end
-
-# 12.times do |i|
-#   Grade.create!(
-#     name: "Grade #{i+1}" 
-#     )
-# end
-
-# 12.times do |i|
-#   Account.create!(
-#     student_id: "#{i+1}",
-#     grade_id: "#{i+1}",
-#     amount: "#{i*5+50}")
-# end
-
-# 12.times do |i|
-#   Fee.create!(grade_id: "#{i+1}",
-#   price: "#{i*5+50}")
-  
-# end
+         12.times do |i|
+              Grade.create!(
+                   name: "Grade {i+1}" 
+                   )
+               end
+          12.times do |i|
+                Fee.create!(grade_id: "{i+1}",
+                              price: "{i*5+50}")
+                  
+                 end
 
 
 
 
 
+          12.times do |i|
+               Account.create!(
+                    student_id: "{i+1}",
+                    grade_id: "{i+1}",
+                    amount: "{i*5+50}")
+                end
+
+          
 
