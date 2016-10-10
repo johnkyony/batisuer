8d6a9bc14ff4dc30ab821d606091585ab564f8df
John kyony <jkyony@gmail.com>
2016-10-10 15:05:46 +0200

    fix the redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   rescue_from CanCan::AccessDenied do |exception|
     flash[:warning] = exception.message
-      redirect_to root_path
+      redirect_to :back
   end
 
 end
